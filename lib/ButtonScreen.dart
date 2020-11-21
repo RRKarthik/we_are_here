3ce4cbed90058f348579983c889d4179644b970a
KarthikD77 <raviramkarthik7@gmail.com>
2020-11-21 08:50:46 +0530

    button screen

--- lib/ButtonScreen.dart
+++ lib/ButtonScreen.dart
@@ -3,23 +3,48 @@ import 'package:flutter/material.dart';
 class ButtonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffffd5cd),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: RawMaterialButton(
-              onPressed: (){},
-              elevation: 2.0,
-              fillColor: Colors.black,
-              child: Icon(Icons.warning,size: 50.0,color: Colors.white,),
-              shape: CircleBorder(),
-            ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffffd5cd),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 30.0,
+              ),
+              Row (
+                children: [
+                  Expanded(child: SizedBox(width: double.infinity,)),
+                  IconButton(
+                    icon: Icon(Icons.chat,size: 45.0,color: Colors.black,),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 200.0,
+              ),
+              Center(
+                  child: RaisedButton(
+                disabledColor: Colors.black,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 100.0, vertical: 50.0),
+                  child: Text(
+                    "Help",
+                    style: TextStyle(fontSize: 50.0,color: Colors.white),
+                  ),
+                ),
+                elevation: 30.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )),
+            ],
           ),
-        ],
+        ),
       ),
-
     );
   }
 }
